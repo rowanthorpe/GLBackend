ca310a7aa87bab5822b80d5bc99d1e65b50121f9
Giovanni evilaliv3 Pellerano <giovanni.pellerano@evilaliv3.org>
2013-09-04 14:02:40 +0200

    Bump to version 2.24.14

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ def download_glclient():
 
 def verify_glclient():
     print "[+] Checking GLClient hash..."
-    glclient_hash = "87586026d167026093f254a3cabab7f8261bf0cc3478c4155510cee2"
+    glclient_hash = "2a117e2b15f0b50d6c3206bc20fff981948093c286b1dcc8c54095cd"
     with open('glclient.zip') as f:
         h = hashlib.sha224(f.read()).hexdigest()
         if not h == glclient_hash:
